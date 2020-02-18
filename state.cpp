ee37b726830121ed00639ae8e25e9305b0a9434e
shenlaoshi <shenshuyaonoe@gmail.com>
2020-02-18 10:36:20 +0100

    add states

--- state.cpp
+++ state.cpp
@@ -27,3 +27,79 @@ bool E0::transition(automate & automate, symbole * s) {
 }
 
 //E1 class
+E1::E1() {
+
+}
+
+E1::~E1() {
+
+}
+
+bool E1::transition(automate & automate, symbole * s) {
+    switch (*s) {
+        case PLUS:
+            automate.decalage(s, new E4);
+            break;
+            
+        case MULT:
+            automate.decalage(s, new E5);
+            break;
+            
+        case FIN:
+            return true;
+            
+        default:
+            return false;
+    }
+}
+
+//E2 class
+E2::E2() {
+
+}
+
+E2::~E2() {
+
+}
+
+bool E2::transition(automate & automate, symbole * s) {
+    switch (*s) {
+        case INT:
+            automate.decalage(s, new E3);
+            break;
+            
+        case OPENPAR:
+            automate.decalage(s, new E2);
+            break;
+            
+        case EXPR:
+            automate.decalage(s, new E6);
+            break;
+            
+        default:
+            return false;
+    }
+}
+
+//E3 class
+E3::E3() {
+
+}
+
+E3::~E3() {
+
+}
+
+bool E3::transition(automate & automate, symbole * s) {
+    switch (*s) {
+        case PLUS:
+        case MULT:
+        case CLOSEPAR:
+        case FIN:
+            
+            break;
+            
+        default:
+            return false;
+    }
+}
--- state.h
+++ state.h
@@ -19,3 +19,27 @@ class E0 : public state
         ~E0();
         bool transition(automate & automate, symbole * s);
 };
+
+class E1 : public state
+{
+    public:
+        E1();
+        ~E1();
+        bool transition(automate & automate, symbole * s);
+};
+
+class E2 : public state
+{
+    public:
+        E2();
+        ~E2();
+        bool transition(automate & automate, symbole * s);
+};
+
+class E3 : public state
+{
+    public:
+        E3();
+        ~E3();
+        bool transition(automate & automate, symbole * s);
+};
